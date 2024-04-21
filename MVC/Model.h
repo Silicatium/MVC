a87ddf8e8592754bbbf1fc433ed681149ff817b7
Marat <yanbaev.marat2004@mail.ru>
2024-04-21 23:14:47 +0500

    Переделана логика изменений значений.

--- MVC/Model.h
+++ MVC/Model.h
@@ -11,7 +11,7 @@ public:
 		if (A >= 0) {
 			if (A <= B) this->A = A;
 			else if (A <= C) { set_value_B(A); set_value_A(A); }
-			else if (A <= 100) { set_value_C(A); set_value_B(A); set_value_A(A); }
+			else if (A <= 100) { set_value_B(A, true); set_value_A(A); }
 		}
 		else this->A = 0;
 		observer->Invoke(this, nullptr);
@@ -20,11 +20,15 @@ public:
 		if (B >= A && B <= C) this->B = B;
 		observer->Invoke(this, nullptr);
 	}
+	void set_value_B(int B, bool flag) {
+		if (flag) { set_value_C(B); set_value_B(B); }
+		else { set_value_A(B); set_value_B(B); }
+	}
 	void set_value_C(int C) {
 		if (C <= 100) {
 			if (C >= B) this->C = C;
 			else if (C >= A) { set_value_B(C); set_value_C(C); }
-			else if (C >= 0) { set_value_A(C); set_value_B(C); set_value_C(C); }
+			else if (C >= 0) { set_value_B(C, false); set_value_C(C); }
 		}
 		else this->C = 100;
 		observer->Invoke(this, nullptr);
